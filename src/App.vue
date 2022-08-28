34d492002386706369abefbbae624e921a8cc773
Osmani Diaz <osmadc005@gmail.com>
2022-08-28 00:01:59 -0400

    basic setup2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>This is a test H1</h1>
+  <h1
+    class="mt-8 bg-slate-600 text-center hover:bg-sky-700 hover:text-lg hover:cursor-pointer"
+  >
+    This is a test H1
+  </h1>
 </template>
 
 <script>
---
+++ src/playground.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple", "Banana", "Peach"];
+const veggies = ["Tomatoe", "Cucumber"];
+
+const combo = [...fruits, veggies[1]];
+
+console.log(combo);
