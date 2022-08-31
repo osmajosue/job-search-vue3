6c2c8ede4bf1cc2ef4588312365ad17429d72d16
Osmani Diaz <osmadc005@gmail.com>
2022-08-31 01:45:53 -0400

    testing the nav items

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -7,6 +7,24 @@ describe("MainNav", () => {
     // await wrapper.setData({
     //   company: "Job Page",
     // });
-    expect(wrapper.text()).toMatch("Diaz Carreers");
+    expect(wrapper.text()).toMatch("Diaz Careers");
+  });
+
+  it("Displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const expectedNavItems = [
+      "Teams",
+      "Locations",
+      "Life at Diaz Careers",
+      "How We Hire",
+      "Students",
+      "Jobs",
+    ];
+    // wrapper.find returns the first encounter
+    const navMenuItems = wrapper.findAll("[data-test='main-nav-list-item']");
+    const navMenuItemsText = navMenuItems.map((menuItem) => {
+      return menuItem.text();
+    });
+    expect(navMenuItemsText).toEqual(expectedNavItems);
   });
 });
