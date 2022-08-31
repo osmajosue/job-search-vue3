5f09d6cfae8104036a0afecd4878c9adf4d259a5
Osmani Diaz <osmadc005@gmail.com>
2022-08-31 00:55:28 -0400

    v-bind and v-for usage in the navbar

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,23 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a href="/" class="flex items-center h-full text-xl">{{ company }}</a>
+        <a :href="url" class="flex items-center h-full text-xl">{{
+          company
+        }}</a>
+
+        <nav class="h-full ml-12">
+          <ul class="flex h-full p-0 m-0 list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="h-full ml-9 first:ml-0"
+            >
+              <a href="" class="flex items-center h-full py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -15,7 +31,16 @@ export default {
   name: "MainNav",
   data() {
     return {
-      company: "Diaz Carreers",
+      company: "Diaz Careers",
+      url: "https://careers.google.com",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at Diaz Careers",
+        "How We Hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
