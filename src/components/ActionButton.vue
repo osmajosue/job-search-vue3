31fb98c7209190effb92a3f624ebfc401633a6a6
Osmani Diaz <osmadc005@gmail.com>
2022-08-31 15:01:52 -0400

    adding action button component -Tailwind styles for it

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="bg-brand-blue-1 px-4 py-2.5 text-white text-xs font-semibold rounded border-0 hover:shadow-blue"
+  >
+    Sign in
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+};
+</script>
