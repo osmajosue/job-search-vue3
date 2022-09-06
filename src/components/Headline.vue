8e85bea2ee2dd7383d9fbd6def5797c0598e3b3a
Osmani Diaz <osmadc005@gmail.com>
2022-09-06 02:11:03 -0400

    Creating Hero component - Headline component

---
+++ src/components/Headline.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ verb }} for everyone</h1>
+  <h2>Find your next Job at Diaz Careers</h2>
+</template>
+
+<script>
+export default {
+  name: "Headline",
+  data() {
+    return {
+      verb: "Code",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const verbs = ["Build", "Code", "Design", "Create"];
+        const currentVerbIndex = verbs.indexOf(this.verb);
+        const nextVerbIndex = (currentVerbIndex + 1) % 4;
+        const nextVerb = verbs[nextVerbIndex];
+        this.verb = nextVerb;
+      }, 5000);
+    },
+  },
+};
+</script>
